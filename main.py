4b93d178ee013d62f85e74528bfbe4ab57985e34
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-06-22 11:41:32 -0400

    Basic Analysis

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ df['date'] = pd.to_datetime(df['date'])
 
 null_vals = df.isnull().sum()
 
+total_spending = df['amount'].sum()
+avg_transaction = df['amount'].mean()
+biggest_spending = df['amount'].max()
+biggest_item = df.loc[df['amount'].idxmax(), 'item']
+
 # print(df.head())
-print(null_vals)
+# print(null_vals)
+print(total_spending)
+print(avg_transaction)
+print(biggest_item)
