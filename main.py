3b79b059e4d1b215db38e22aaea12ded9d97fa2d
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-06-22 12:00:53 -0400

    Daily Spending

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ biggest_spending = df['amount'].max()
 biggest_item = df.loc[df['amount'].idxmax(), 'item']
 category_total = df.groupby('category')['amount'].sum().sort_values(ascending=False)
 
+biggest_spending_day = df.loc[df['amount'].idxmax(), 'date'].day_name()
+
+df['is_weekend'] = df['date'].dt.day_of_week>=5
+
+weekend_vs_weekday = df.groupby('is_weekend')['amount'].sum()   
+
+
+# print(weekend_vs_weekday)
 # print(df.head())
 # print(null_vals)
 # print(total_spending)
 # print(avg_transaction)
 # print(biggest_item)
-print(category_total)
+# print(category_total)
+# print(biggest_spending_day)
